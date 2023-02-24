d5cbe9125fdbec10613d735eef59ea888bb9ca2d
Victor Calebe Cavalcante de Macedo <calebe.m@academico.ifg.edu.br>
2023-02-24 08:31:51 -0300

    Add files via upload

---
+++ dependences.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriverchromedriver --version
+curl https://intoli.com/install-google-chrome.sh | bash
+sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+google-chrome --version && which google-chrome
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install selenium
